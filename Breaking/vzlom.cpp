606e050bd91ccf1ea8bedbbcc8fdce028e27302d
KatalinaE <55049564+KatalinaE@users.noreply.github.com>
2020-09-21 17:17:30 +0300

    Update vzlom.cpp

--- Breaking/vzlom.cpp
+++ Breaking/vzlom.cpp
@@ -1,19 +1,19 @@
-#include <iostream>
 #include <stdio.h>
-#include <assert.h>
-
-using namespace std;
 
 int main()
 {
 	FILE *in = NULL;
 	in = fopen("vzlom(1).com", "rb+");
-	assert(in);
+	if (in == NULL) {
+		return -1;
+	}
 	
 	int pos = 0;
 	pos = 0x23;
-	int fseek_error = fseek(in, pos, SEEK_SET);
-	assert(fseek_error != -1);
+	int fseek_status = fseek(in, pos, SEEK_SET);
+	if (fseek_status == -1) {
+		return -1;
+	}
 	
 	int symb = 0;
 	symb = 0x74;
